5e18eb6437215af441b904a20d1bd1a3d30ca300
Waibhav Jha <waibhav204@gmail.com>
2024-09-23 23:27:28 +0530

    Initial commit

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+        <h1 className="created-by">by <a href="https://twitter.com/waibhavjha" target="_blank" rel="noopener noreferrer">@waibhavjha</a></h1>
+        </footer>
+    )
+}
